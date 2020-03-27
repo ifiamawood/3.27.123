c23f8f020fedf0fe496856b00c298b8303fe949d
ifiamawood <821371350@qq.com>
2020-03-27 14:49:02 +0800

    111

--- 3.27.1/3.27.1.cpp
+++ 3.27.1/3.27.1.cpp
@@ -13,16 +13,18 @@ int main()
 {
 	VideoCapture cap;
 	cap.open(0);
-	cv::Mat output1,input,output2,output3;
+	cv::Mat output1,input,output2,output31,output32;
 	cap>>input;
 	cv::medianBlur(input, output1, 3);
 	cv::blur(input, output2, Size(5,5), Point(-1, -1), BORDER_DEFAULT);
-	cv::GaussianBlur(input, output3, Size(5,5), 0, BORDER_DEFAULT);
+	cv::GaussianBlur(input, output31, Size(5,5), 1, 20);
+	cv::GaussianBlur(input, output32, Size(5, 5), 20, 1);
 	imshow("中值", output1);
 	waitKey(0);
 	imshow("均值blur", output2);
 	waitKey(0);
-	imshow("均值gaussianblur", output3);
+	imshow("均值gaussianblurx", output31);
+	imshow("均值gaussianblury", output32);
 	waitKey(0);
     return 0;
 }
